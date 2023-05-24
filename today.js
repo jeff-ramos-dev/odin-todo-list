d97970a18be543fdca6aeab6554a3e1652beace7
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-05-24 14:29:01 -0700

    Refactor to use getTodayList function

--- today.js
+++ today.js
@@ -1,5 +1,5 @@
 export default function todayPage(listOfLists) {
-    const allLists = listOfLists.getAllLists()
+    const todayList = listOfLists.getTodayList()
 
     const title = document.createElement('h1');
     title.textContent = 'TODAY'
@@ -7,38 +7,28 @@ export default function todayPage(listOfLists) {
     const container = document.createElement('div');
     container.appendChild(title);
 
-    for (const list in allLists) {
-        const numOfItems = allLists[list].length
-        for (let i = 0; i < numOfItems; i++) {
-            const curr = allLists[list][i]
-            const date = curr['dueDate']
-            const currDay = Date().split(' ').splice(1, 3).join(' ');
-            if (date === currDay) {
-                const todayTask = document.createElement('div');               
-                todayTask.classList.add('task');
-                const todayTitle = document.createElement('p');
-                todayTitle.classList.add('title', 'prop');
-                todayTitle.textContent = curr.title
-                const todayDesc = document.createElement('p');
-                todayDesc.classList.add('desc', 'prop');
-                todayDesc.textContent = curr.description
-                const todayPriority = document.createElement('p');
-                todayPriority.classList.add('priority', 'prop');
-                todayPriority.textContent = `Priority: ${curr.priority}`
+    for (const item in todayList) {
+        const todayTask = document.createElement('div');               
+        todayTask.classList.add('task');
+        const todayTitle = document.createElement('p');
+        todayTitle.classList.add('title', 'prop');
+        todayTitle.textContent = todayList[item].title
+        const todayDesc = document.createElement('p');
+        todayDesc.classList.add('desc', 'prop');
+        todayDesc.textContent = todayList[item].description
+        const todayPriority = document.createElement('p');
+        todayPriority.classList.add('priority', 'prop');
+        todayPriority.textContent = `Priority: ${todayList[item].priority}`
 
-                todayTask.style.backgroundColor = curr.complete ? '#0f6e22': '#7a0610';
-                todayTask.addEventListener('click', e => {
-                    listOfLists.updateComplete(curr, !curr.complete)
-                    todayTask.style.backgroundColor = curr.complete ? '#0f6e22' : '#7a0610'
-                })
-
-
-                todayTask.appendChild(todayTitle);
-                todayTask.appendChild(todayDesc);
-                todayTask.appendChild(todayPriority);
-                container.appendChild(todayTask);
-            }
-        }
+        todayTask.style.backgroundColor = todayList[item].complete ? '#0f6e22': '#7a0610';
+        todayTask.addEventListener('click', e => {
+            listOfLists.updateComplete(todayList[item], !todayList[item].complete)
+            todayTask.style.backgroundColor = todayList[item].complete ? '#0f6e22' : '#7a0610'
+        })
+        todayTask.appendChild(todayTitle);
+        todayTask.appendChild(todayDesc);
+        todayTask.appendChild(todayPriority);
+        container.appendChild(todayTask);
     }
 
     return container 
